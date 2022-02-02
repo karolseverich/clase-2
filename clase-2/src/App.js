76550ee6dc5bf19c95cc80ec285d3f921066ca51
Karol Severich <karolsev.ks@gmail.com>
2022-02-01 23:11:46 -0300

    cuarto commit

--- clase-2/src/App.js
+++ clase-2/src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
+
     <div className="App">
+      <Navbar />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
